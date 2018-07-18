ba49536bde22f9b5b9f72192e6007a4a74c16384
yeungpirate <yeungpirate@gmail.com>
2018-07-17 19:08:41 -0700

    converted over to use react-bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,9 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NoMatch from './pages/NoMatch';
 import Nav from './components/Nav';
-import AllRecipes from './pages/viewAllRecipes'
+import AllRecipes from './pages/viewAllRecipes';
+import Likes from './pages/Likes';
+import Following from './pages/Following';
 
 const App = () => (
   <Router>
@@ -12,6 +14,9 @@ const App = () => (
       <Switch>
         {/* <Route exact path='/' component={Users} /> */}
         <Route exact path='/recipes' component={AllRecipes} />
+        <Route exact path='/likes' component={Likes} />
+        <Route exact path='/following' component={Following} />
+        <Route exact path='/register' component={Register} />
         {/* <Route exact path='/users/:id' component={Users} /> */}
         <Route component={NoMatch} />
       </Switch>
