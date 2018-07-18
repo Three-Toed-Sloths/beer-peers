d962806dd5938beddf7ab372ddcfd465af7d101e
Nirav Patel <nrvpatel03@gmail.com>
2018-07-18 10:18:19 -0700

    secondary nav

--- client/src/components/SecondaryNav/SecondaryNav.js
+++ client/src/components/SecondaryNav/SecondaryNav.js
@@ -2,32 +2,17 @@ import React from "react";
 import Grid from 'react-bootstrap/lib/Grid';
 import Col from 'react-bootstrap/lib/Col';
 import Row from 'react-bootstrap/lib/Row';
-import Navbar from 'react-bootstrap/lib/Navbar';
-import Nav from 'react-bootstrap/lib/Nav';
-import NavItem from 'react-bootstrap/lib/NavItem';
 import "./SecondaryNav.css";
 
 const SecondaryNav = () => (
-    <Grid>
-        <Row>
-            <Navbar>
-                <Nav>
-                    <NavItem eventKey={1} href="#">
-                        <h1>Overview</h1>
-                    </NavItem>
-                    <NavItem eventKey={2} href="#">
-                        <h1>Recipes</h1>
-                    </NavItem>
-                    <NavItem eventKey={3} href="#">
-                        <h1>Follows</h1>
-                    </NavItem>
-                    <NavItem eventKey={4} href="#">
-                        <h1>Likes</h1>
-                    </NavItem>
-                </Nav>
-            </Navbar>
-        </Row>
-    </Grid>
+<Grid className="secondaryNav">
+    <Row className="secondaryNavRow">
+        <Col className="secondaryCols" sm={3}><h1 className="secondaryColText">Overview</h1></Col>
+        <Col className="secondaryCols" sm={3}><h1 className="secondaryColText">Following</h1></Col>
+        <Col className="secondaryCols" sm={3}><h1 className="secondaryColText">Likes</h1></Col>
+        <Col className="secondaryCols" sm={3}><h1 className="secondaryColText">Recipes</h1></Col>
+    </Row>
+</Grid>
 );
 
 export default SecondaryNav;
