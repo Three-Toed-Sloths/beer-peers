b56c9712a5e198118545fe8efaadf0e6b2107b6b
Nick Clear <nickclear22@gmail.com>
2018-07-17 17:34:29 -0700

    rearranged form groups

--- client/src/components/RecipeForm/RecipeForm.js
+++ client/src/components/RecipeForm/RecipeForm.js
@@ -39,116 +39,146 @@ class RecipeForm extends Component {
             <Row className="show-grid">
                 <Col xs={12} md={12}>
                     <form>
-                        <FormGroup
-                        controlId="formBasicText"
-                        // validationState={this.getValidationState()}
-                        >
-                            <ControlLabel>Recipe Name</ControlLabel>
-                            <FormControl
-                                type="text"
-                                value={this.state.value}
-                                placeholder="Enter name"
-                                onChange={this.handleChange}
-                            />
-                            <FormControl.Feedback />
-                            <HelpBlock>Validation is based on string length.</HelpBlock>
-                        </FormGroup>
-                        <FormGroup controlId="formControlsSelect">
-                            <ControlLabel>Select Beer Style</ControlLabel>
-                            <FormControl componentClass="select">
-                                {beerStyles.map(style => (
-                                    <option value={style}>{style}</option>
-                                ))}
-                            </FormControl>
-                        </FormGroup>
 
-                        <FormGroup controlId="formControlsTextarea">
-                            <ControlLabel>Recipe Description</ControlLabel>
-                            <FormControl componentClass="textarea" placeholder="Description" />
-                        </FormGroup>
+
+                        <Row>
+                            <Col xs={6}>
+                                <FormGroup controlId="formBasicText">
+                                    <ControlLabel>Recipe Name</ControlLabel>
+                                    <FormControl
+                                        type="text"
+                                        value={this.state.value}
+                                        placeholder="Enter name"
+                                        onChange={this.handleChange}
+                                    />
+                                    <FormControl.Feedback />
+                                    <HelpBlock>Validation is based on string length.</HelpBlock>
+                                </FormGroup>
+                            </Col>
+                            <Col xs={6}>
+                                <FormGroup controlId="formControlsSelect">
+                                    <ControlLabel>Select Beer Style</ControlLabel>
+                                    <FormControl componentClass="select">
+                                        {beerStyles.map(style => (
+                                            <option value={style}>{style}</option>
+                                        ))}
+                                    </FormControl>
+                                </FormGroup>
+                            </Col>
+                        </Row>
+
+                        
+                        <Row>
+                            <Col xs={4}>
+                                <Row>
+                                    <Col xs={6}>
+                                        <FormGroup controlId="batch" >
+                                            <ControlLabel>Batch Volume</ControlLabel>
+                                            <InputGroup>
+                                                <FormControl type="number" id="batchVol" placeholder="Batch"/>
+                                                <DropdownButton
+                                                    componentClass={InputGroup.Button}
+                                                    id="input-dropdown-addon"
+                                                    title="Units"
+                                                >
+                                                    {units.map(unit => (
+                                                        <MenuItem key={unit} value={unit}>{unit}</MenuItem>
+                                                    ))}
+                                                </DropdownButton>
+                                            </InputGroup>
+                                        </FormGroup>
+                                    </Col>
+                                    <Col xs={6}>
+                                        <FormGroup controlId="abv">
+                                            <ControlLabel>ABV</ControlLabel>
+                                            <InputGroup>
+                                                <FormControl id="abvInput" type="number" placeholder="Enter ABV"/>
+                                                <InputGroup.Addon>%</InputGroup.Addon>
+                                            </InputGroup>
+                                        </FormGroup>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col xs={6}>
+                                        <FormGroup controlId="originalGravity" >
+                                            <ControlLabel>Original Gravity</ControlLabel>
+                                            <FormControl
+                                                id="ogInput"
+                                                type="number"
+                                                placeholder="Enter OG"
+                                            />
+                                        </FormGroup>
+                                    </Col>
+                                    <Col xs={6}>
+                                        <FormGroup controlId="finalGravity" >
+                                            <ControlLabel>Final Gravity</ControlLabel>
+                                            <FormControl
+                                                id="fgInput"
+                                                type="number"
+                                                placeholder="Enter FG"
+                                            />
+                                        </FormGroup>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col xs={6}>
+                                        <FormGroup controlId="preboil" >
+                                            <ControlLabel>Preboil Volume</ControlLabel>
+                                            <InputGroup>
+                                            <FormControl type="number" id="preBoilVol" placeholder="Preboil"/>
+                                                <DropdownButton
+                                                    componentClass={InputGroup.Button}
+                                                    id="input-dropdown-addon"
+                                                    title="Units"
+                                                >
+                                                    {units.map(unit => (
+                                                        <MenuItem key={unit} value={unit}>{unit}</MenuItem>
+                                                    ))}
+                                                </DropdownButton>
+                                            </InputGroup>
+                                        </FormGroup>
+                                    </Col>
+                                    <Col xs={6}>
+                                        <FormGroup controlId="boilTime">
+                                            <ControlLabel>Boil Time</ControlLabel>
+                                            <InputGroup>
+                                                <FormControl type="number" placeholder="Boil"/>
+                                                <InputGroup.Addon>min</InputGroup.Addon>
+                                            </InputGroup>
+                                        </FormGroup>
+                                    </Col>
+                                </Row>
+
+                            </Col>
+                            <Col xs={8}>
+                                <FormGroup controlId="recipeDescription">
+                                    <ControlLabel>Recipe Description</ControlLabel>
+                                    <FormControl componentClass="textarea" id="recipeDescription" placeholder="Description" />
+                                </FormGroup>
+                            </Col>
+                        </Row>
+
+                        
                         <FormGroup controlId="formControlsTextarea">
                             <ControlLabel>Recipe Instructions</ControlLabel>
                             <FormControl componentClass="textarea" placeholder="Instructions" />
                         </FormGroup>
 
-                        <Col xs={2}>
-                            <FormGroup controlId="abv">
-                                <ControlLabel>ABV</ControlLabel>
-                                <InputGroup>
-                                    <FormControl id="abvInput" type="number" placeholder="Enter ABV"/>
-                                    <InputGroup.Addon>%</InputGroup.Addon>
-                                </InputGroup>
-                            </FormGroup>
-                        </Col>
+                        
 
-                        <FormGroup controlId="gravities" >
-                            <Col xs={2}>
-                                <ControlLabel>Original Gravity</ControlLabel>
-                                <FormControl
-                                    id="ogInput"
-                                    type="number"
-                                    placeholder="Enter OG"
-                                />
-                            </Col>
-                            <Col xs={2}>
-                                <ControlLabel>Final Gravity</ControlLabel>
-                                <FormControl
-                                    id="fgInput"
-                                    type="number"
-                                    placeholder="Enter FG"
-                                />
-                            </Col>
-                        </FormGroup>
                         
-                        <Row>  
-                            <Col xs={2}>
-                                <FormGroup controlId="batch" >
-                                    <ControlLabel>Batch Volume</ControlLabel>
-                                    <InputGroup>
-                                        <FormControl type="number" id="batchVol" placeholder="Enter Batch Volume"/>
-                                        <DropdownButton
-                                            componentClass={InputGroup.Button}
-                                            id="input-dropdown-addon"
-                                            title="Units"
-                                        >
-                                            {units.map(unit => (
-                                                <MenuItem key={unit} value={unit}>{unit}</MenuItem>
-                                            ))}
-                                        </DropdownButton>
-                                    </InputGroup>
-                                </FormGroup>
-                            </Col>
-                        </Row>
+                            
+                            
                         
                         
-                        <Row>
-                            <Col xs={2}>
-                                <FormGroup controlId="preboil" >
-                                    <ControlLabel>Preboil Volume</ControlLabel>
-                                    <InputGroup>
-                                    <FormControl type="number" id="preBoilVol" placeholder="Enter PreBoil Volume"/>
-                                        <DropdownButton
-                                            componentClass={InputGroup.Button}
-                                            id="input-dropdown-addon"
-                                            title="Units"
-                                        >
-                                            {units.map(unit => (
-                                                <MenuItem key={unit} value={unit}>{unit}</MenuItem>
-                                            ))}
-                                        </DropdownButton>
-                                    </InputGroup>
-                                </FormGroup>
-                            </Col>
-                        </Row>
-                        <Col xs={2}>
-                            <FormGroup controlId="boilTime">
-                                <ControlLabel>Boil Time</ControlLabel>
-                                <InputGroup>
-                                    <FormControl type="text" />
-                                    <InputGroup.Addon>min</InputGroup.Addon>
-                                </InputGroup>
-                            </FormGroup>
-                        </Col>
+                        
+                        
+                     
+                            
+                  
+                
+                            
+                    
                        
      
                     </form>
