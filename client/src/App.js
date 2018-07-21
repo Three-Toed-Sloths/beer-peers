af2200ecf9736202ec071f264c167868bc91ddd1
Nick Clear <nickclear22@gmail.com>
2018-07-21 15:00:50 -0700

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,4 @@
 
-// import React from 'react';
-// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-// import NoMatch from './pages/NoMatch';
-// // import Nav from './components/Nav';
-// import Wrapper from './components/Wrapper';
-// import AllRecipes from './pages/ViewAllRecipes';
-// import ViewRecipe from './pages/ViewRecipe';
-// import AddRecipe from './pages/AddRecipe';
-
-// const App = () => (
-//   <Wrapper>
-//     <Router>
-//         {/* <Nav /> */}
-//         <Switch>
-//           {/* <Route exact path='/' component={Users} /> */}
-//           <Route exact path='/recipes' component={AllRecipes} />
-//           <Route exact path='/recipes/new' component={AddRecipe} />
-//           <Route exact path='/recipes/:id' component={ViewRecipe} />
-          
-          
-//           <Route component={NoMatch} />
-//         </Switch>
-//     </Router>
-//   </Wrapper>
-
 
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
