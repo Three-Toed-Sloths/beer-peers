82d7da9a41ddefccfe551317ac413db11017e910
yeungpirate <yeungpirate@gmail.com>
2018-07-23 18:10:12 -0700

    resolved conflicts and prep for pull request to dev

--- client/src/pages/Likes/Likes.js
+++ client/src/pages/Likes/Likes.js
@@ -1,27 +1,8 @@
-// import React from 'react';
-
 import React, { Component } from 'react';
 import API from '../../utils/userAPI';
 
 import {Grid, Row, Col} from 'react-bootstrap';
 import './Likes.css';
-
-// const Likes = () => {
-//     return (
-//         <Row>
-//             <Col xs={12} className='likesCol'>
-//                 <div className='likesParallax'>
-//                     <div className='likesContainer'>
-//                         <h1 className='likesHeader'>Likes</h1>
-//                         <hr />
-//                     </div>
-//                 </div>
-//             </Col>
-//         </Row>
-//         // <Grid>
-//         // </Grid>
-//     );
-// }
 
 class Likes extends Component {
     state = {
@@ -29,7 +10,6 @@ class Likes extends Component {
         likes: []
     }
 
-    
     componentDidMount() {
         this.getLikes(this.state.id);
     }
