78da34b29342e3b45c9bb4d3215f53a17fdc61f9
Nick Clear <nickclear22@gmail.com>
2018-07-24 21:49:28 -0700

    first attempt at likes page. display recipeCards

--- client/src/pages/Likes/Likes.js
+++ client/src/pages/Likes/Likes.js
@@ -4,6 +4,7 @@ import API from '../../utils/userAPI';
 import {Grid, Row, Col} from 'react-bootstrap';
 import Wrapper from '../../components/Wrapper';
 import SecondaryNav from '../../components/SecondaryNav';
+import RecipeCard from '../../components/RecipeCard';
 import './Likes.css';
 
 class Likes extends Component {
@@ -22,6 +23,7 @@ class Likes extends Component {
             this.setState({
                 likes:res.data.social.favorites
             })
+            console.log(this.state.likes[0].name + ' likes')
         })
     }
 
@@ -41,14 +43,27 @@ class Likes extends Component {
                 </div>
                 <Wrapper>
                     <Row>
-                        <Col xs={12}>
-                            {this.state.likes.map(like => (
-                                <h1>{like.name}</h1>
-                            ))}
-                        </Col>
+                        <SecondaryNav iden={this.state.id}/>
                     </Row>
                     <Row>
-                        <SecondaryNav />
+                        <Col xs={12}>
+                            {this.state.likes.map(recipe => (
+                                <RecipeCard
+                                id={recipe._id}
+                                    name={recipe.name}
+                                    style={recipe.style}
+                                    abv={recipe.specs.abv}
+                                    batchSize={recipe.specs.batch.size}
+                                    batchUnits={recipe.specs.batch.units}
+                                    ibu={recipe.specs.ibu}
+                                    fg={recipe.specs.fg}
+                                    // brewer={recipe.brewer.username}
+                                    // brewerFirstName={recipe.brewer.name.first}
+                                    // brewerLastName={recipe.brewer.name.last}
+                                    description={recipe.description}
+                                />
+                            ))}
+                        </Col>
                     </Row>
                 </Wrapper>
             </div>
