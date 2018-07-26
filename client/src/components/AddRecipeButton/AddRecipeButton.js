0b0307929e72b07927030e34c2d9610b0725c45f
Marco Saglietto <saglietto_marco@yahoo.com>
2018-07-25 22:41:02 -0700

    fixed AddRecipeButton href route

--- client/src/components/AddRecipeButton/AddRecipeButton.js
+++ client/src/components/AddRecipeButton/AddRecipeButton.js
@@ -6,7 +6,7 @@ const AddRecipeButton = props => (
     <Row>
         <Col xs={2} md={4} />
         <Col xs={8} md={4}>
-            <Button id='AddRecipeButton' block href='/recipes/new' bsSize='large'>
+            <Button id='AddRecipeButton' block href={`/personal/${props.id}/recipes/new`} bsSize='large'>
                 Add a Recipe to Your Profile
             </Button>
         </Col>
